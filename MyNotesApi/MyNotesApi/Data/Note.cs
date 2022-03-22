aa486ead53f65bc4b943c026b3487cc721322fd5
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-03-22 10:38:28 +0300

    data layer

---
+++ MyNotesApi/MyNotesApi/Data/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyNotesApi.Data
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedTime { get; set; } = DateTime.Now;
+    }
+}
